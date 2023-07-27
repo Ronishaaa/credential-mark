ce2efa3847720cf9544ce611713ce59f66ccb3a5
Ronisha Shrestha <ronisha.shrestha03gmail.com>
2023-07-27 15:07:03 +0545

    update output value

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,41 @@ import './App.css'
 
 function App() {
 
-  const [input, setInput] = useState({
-    inputText: ""
-  })
-  function handleCopy(){
-    navigator.clipboard.writeText(input.inputText)
-  }
+    const [inputValue, setInputValue] = useState('');
+    const [outputValue, setOutputValue] = useState('');
 
-  function handleChange(event) {
-      const {name, value} = event.target
-      setInput(prevInput => ({
-          ...prevInput,
-          [name]: value
-      }))
+
+    function handleInputValue(event) {
+      const newInputValue = event.target.value;
+      setInputValue(newInputValue);
+      const updatedOutput = newInputValue.replace(/[A-Z]/g, 'X').replace(/[a-z]/g, 'x').replace(/\d/g, 'D');
+      setOutputValue(updatedOutput);
+    }
+
+  function handleCopy() {
+    navigator.clipboard.writeText(inputValue);
   }
 
   return (
-          <div className="container">
-            <h1>Input :</h1>
-            <input 
-                type="text"
-                className="input"
-                name="inputText"
-                value={input.inputText}
-                onChange={handleChange}
-            />
-            <h1>Output :</h1>
-            <input 
-                type="text"
-                className="input"
-                value={input.inputText}
-                readOnly
-            />
-            <button onClick={handleCopy}>copy</button>
-              
-          </div>
+    <div className="container">
+      <h1>Input :</h1>
+      <input
+        type="text"
+        className="input"
+        value={inputValue}
+        onChange={handleInputValue}
+      />
+      <h1>Output :</h1>
+      <input
+        type="text"
+        className="input"
+        value={outputValue}
+        readOnly
+      />
+      <button className="button" onClick={handleCopy}></button>
+    </div>
   )
+
 }
 
 
